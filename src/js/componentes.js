6303167a93867f90ef520e9ee171b648a415728d
Samuel Narciso <SamuelNarciso3280@gmail.com>
2020-06-24 19:04:54 -0500

    listo para produccion

--- src/js/componentes.js
+++ src/js/componentes.js
@@ -5,27 +5,19 @@ import { listaDatos } from '../index'
 const campoMostrarDatos = document.querySelector('#campo-mostrar-datos');
 const btnAgregar = document.querySelector('.agregar');
 const cajaEntrada = document.querySelector('.caja-entrada');
-const btnBorrar = document.querySelector('.borrar');
+const seleccionAzar = document.querySelector('#seleccionar');
+const resultado = document.querySelector('#caja-resultado');
+
 
 export const crearDatoHtml = (datoRecibido) => {
-    console.log('------------Datos-------------');
-    console.log(datoRecibido.dato);
-    console.log(datoRecibido.id);
-    console.log(datoRecibido.color);
-    console.log('------------Datos-------------');
-
-
     const htmlDato = `
     <li class="dato-ingresado" data-id=" ${datoRecibido.id} ">
     <div class="contenido" style="background-color: ${datoRecibido.color};">
 
     <label class="texto-duro">${datoRecibido.dato}</label>
-
         <button class="btn borrar"> x </button>
-
     </div>
-    </li>
-    `;
+    </li> `;
 
     const div = document.createElement('div');
     div.innerHTML = htmlDato;
@@ -37,11 +29,11 @@ export const crearDatoHtml = (datoRecibido) => {
 }
 
 const clickAgregar = () => {
-
     const nuevoDatoRecibido = new Dato(cajaEntrada.value);
     listaDatos.nuevoDato(nuevoDatoRecibido);
     crearDatoHtml(nuevoDatoRecibido);
     cajaEntrada.value = '';
+    console.log(listaDatos);
 
 
 }
@@ -64,8 +56,43 @@ cajaEntrada.addEventListener('keyup', (event) => {
 
 });
 
-btnBorrar.addEventListener('click', (event) => {
+campoMostrarDatos.addEventListener('click', (event) => {
+    const nombreElemento = event.target.localName;
     const botonDatoPresionado = event.target.parentElement.parentElement;//Obtiene la referencia del elemento en el que se encuentra.
     const idDatoBotonPresionado = botonDatoPresionado.getAttribute('data-id');
-    listaDatos.eliminarDato(idDatoBotonPresionado);
+
+    if (nombreElemento.includes('button')) {//presiono la x para borrar el elemento.
+        listaDatos.eliminarDato(idDatoBotonPresionado);
+        campoMostrarDatos.removeChild(botonDatoPresionado);
+        console.log(listaDatos);
+    }
+});
+
+seleccionAzar.addEventListener('click', () => {
+
+    resultado.classList.remove('resultado');
+
+    setTimeout(() => {
+        if (listaDatos.datos.length > 0) {
+            let azar = listaDatos.seleccionarAzar();
+            let datoResultado = listaDatos.datos[azar].dato;
+            let colorResultado = listaDatos.datos[azar].color;
+
+            console.log(resultado.style.backgroundColor = `${colorResultado}`);
+
+
+            resultado.style.backgroundColor = `${colorResultado}`;
+            resultado.innerHTML = (` ${datoResultado} `);
+
+            resultado.classList.add('resultado');
+
+
+        } else {
+            resultado.innerHTML = (`¿ ... ?`);
+
+        }
+    }, 200);
+
+
+
 });
